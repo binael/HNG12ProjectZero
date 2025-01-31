f4461947a22624bd25c74523fe7adb8460cbffa2
binael <nbinael@yahoo.com>
2025-01-31 21:52:30 +0100

    Stage Zero files for hng12

---
+++ Controllers/StageZeroController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HNG12StageZero.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class StageZeroController : Controller
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            StageZero result = new StageZero();
+
+            return new JsonResult(Ok(result));
+        }
+    }
+}
---
+++ StageZero.cs
@@ -0,0 +1,17 @@
+﻿namespace HNG12StageZero
+{
+    public class StageZero
+    {
+        public string Email { get; set; }
+        public string Current_datetime { get; set; }
+        public string Github_url { get; set; }
+
+        public StageZero()
+        {
+            Email = "nbinael@yahoo.com";
+            DateTime today = DateTime.UtcNow;
+            Current_datetime = today.ToString("yyyy-MM-ddTHH-mm-ssZ");
+            Github_url = "https://github.com/binael/HNG12ProjectZero";
+        }
+    }
+}
